8acbfa81c8cf2c9aab19ba5583f408ef9fbe1b32
Timothy Winsley <t-winsley@protonmail.com>
2022-04-18 12:52:58 -0400

    Add function to choose winner
    This adds the base game logic to choose a winner.

--- script.js
+++ script.js
@@ -4,18 +4,42 @@ function computerPlay() {
   return choices[number];
 }
 function playerPlay() {
-  const choice = prompt("Please choose rock, paper, or scissors").toLowerCase();
-  return choice;
+  let choice = prompt("Please choose rock, paper, or scissors").toLowerCase();
+  if (choice !== "rock" && choice !== "paper" && choice !== "scissors") {
+    alert("That was not rock, paper, or scissors. Please try again.");
+    location.reload();
+  } else {
+    return choice;
+  }
+}
+function chooseWinner() {
+  if (computerSelection === "rock" && playerSelection === "paper") {
+    return "winner";
+  } else if (computerSelection === "rock" && playerSelection === "scissors") {
+    return "looser";
+  } else if (computerSelection === "paper" && playerSelection === "scissors") {
+    return "winner";
+  } else if (computerSelection === "paper" && playerSelection === "rock") {
+    return "looser";
+  } else if (computerSelection === "scissors" && playerSelection === "rock") {
+    return "winner";
+  } else if (computerSelection === "scissors" && playerSelection === "paper") {
+    return "looser";
+  } else {
+    return "tie";
+  }
 }
 //console.log(computerPlay());
 function playRound(playerSelection, computerSelection) {
-  console.log(playerSelection, computerSelection);
+  let result = chooseWinner();
+  console.log(result);
   //compare player selection to computer selection
   //return the winner
 }
 const computerSelection = computerPlay();
 const playerSelection = playerPlay();
 console.log(playRound(playerSelection, computerSelection));
+console.log(computerSelection);
 
 //add a game() function to call the functions
 //Add a loop to play multiple rounds.
