77193c2a1bb5c02adddd311c92bf84852f5124ef
Timothy Winsley <t-winsley@protonmail.com>
2022-04-18 11:36:38 -0400

    Add inital rough draft plan

---
+++ script.js
@@ -0,0 +1,18 @@
+function computerPlay() {
+  //choose randomly between rock, paper, and scissors
+  //return the random choice
+}
+
+function playRound(playerSelection, computerSelection) {
+  //prompt player for input
+  //sanatize input to lowercase
+  //compare player selection to computer selection
+  //return the winner
+}
+const computerSelection = computerPlay();
+//console.log(playRound)
+
+//add a game() function to call the functions
+//Add a loop to play multiple rounds.
+//log the result of each loop to the console.
+//log the game winner (Whoever won the most rounds)
